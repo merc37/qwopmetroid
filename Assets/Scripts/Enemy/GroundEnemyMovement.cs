5124c1427240b6bf50ba45a731e5a1ab88db1606
Utkarsh Jha <uj20@scarletmail.rutgers.edu>
2018-11-21 21:25:08 -0700

    Ground Enemy no longer uses RigidBody
    The ground enemy now just gets pushed to ground once when it is set up or as soon as the play button is set and never checks for being grounded again.
    Small bug fix in jump which now allows player to jump variably both on the first jump as well as the second.(This was done through a slight modification of jump code which is now more intuitive and does not use timers)

---
+++ Assets/Scripts/Enemy/GroundEnemyMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundEnemyMovement : MonoBehaviour {
+
+    public FloatReference speed;
+    public Transform[] waypoints;
+
+    private int currentWaypointIndex;
+    private float direction = 0;
+    private bool facingRight = true;
+
+    void Awake()
+    {
+        currentWaypointIndex = 0;
+    }
+
+    void FixedUpdate()
+    {
+        ChangeWayPoint();
+        Move(speed);
+    }
+
+    protected void Move(FloatReference speedToMove)
+    {
+        direction = Mathf.Sign(waypoints[currentWaypointIndex].position.x - transform.position.x);
+        Vector2 velocity = new Vector2(direction * speed.Value, 0);
+        Vector2 moveAmount = velocity * Time.deltaTime;
+        transform.Translate(moveAmount);
+
+        if (facingRight == false && direction > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && direction < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+    private void ChangeWayPoint()
+    {
+        if(direction < 0)
+        {
+            if (transform.position.x <= waypoints[currentWaypointIndex].position.x)
+            {
+                Debug.Log("Changed Way Point to: " + currentWaypointIndex);
+                if (currentWaypointIndex + 1 < waypoints.Length)
+                {
+                    currentWaypointIndex++;
+                }
+                else
+                {
+                    currentWaypointIndex = 0;
+                }
+            }
+        }
+        else
+        {
+            if (transform.position.x >= waypoints[currentWaypointIndex].position.x)
+            {
+                Debug.Log("Changed Way Point to: " + currentWaypointIndex);
+                if (currentWaypointIndex + 1 < waypoints.Length)
+                {
+                    currentWaypointIndex++;
+                }
+                else
+                {
+                    currentWaypointIndex = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/MakingEnemyGrounded.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class MakingEnemyGrounded : MonoBehaviour {
+
+    public LayerMask whatIsGround;
+    private Collider2D collider2d;
+    // Use this for initialization
+
+    private void OnValidate()
+    {
+        collider2d = gameObject.GetComponent<Collider2D>();
+        MakeEnemyGrounded();
+    }
+
+    private void MakeEnemyGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(new Vector3(transform.position.x, transform.position.y - collider2d.bounds.extents.y, transform.position.z), Vector2.down, Mathf.Infinity, whatIsGround);
+        if(hitInfo == true)
+        {
+            float distanceToGround = hitInfo.distance;
+            if(distanceToGround > collider2d.bounds.size.y)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y - distanceToGround, transform.position.z);
+            }
+        }
+    }
+}
