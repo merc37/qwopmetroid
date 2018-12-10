3bb2c97d53bd81a4b8d84f02b4ae5cde886679ed
Utkarsh Jha <uj20@scarletmail.rutgers.edu>
2018-12-10 11:27:44 -0700

    Woring on time boss

---
+++ Assets/Scripts/Boss/TimeBoss/TimeBossMovement.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBossMovement : MonoBehaviour {
+
+    public float skinWidth = 0.01f;
+    public Transform target;
+    public List<GameObject> obstacleTransforms = new List<GameObject>();
+    public LayerMask whatIsObstacle;
+    public GameObject edgePointPrefab;
+    public bool playermoved = false;
+    public Transform instantiatedEdges;
+
+    private Collider2D collider2d;
+
+	// Use this for initialization
+	void Start () {
+        collider2d = GetComponent<Collider2D>();
+        obstacleTransforms.Add(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (playermoved)
+        {
+            foreach (Transform instObj in instantiatedEdges)
+            {
+                instObj.GetComponent<DestroyObject>().Destroy();
+            }
+            FindVisiblePointTo(target, gameObject.transform);
+            playermoved = false;
+        }
+    }
+
+    private void FindVisiblePointTo(Transform target, Transform caster)
+    {
+        Vector2 direction = (target.position - caster.position).normalized;
+        RaycastHit2D hitInfo = Physics2D.Raycast((Vector2)caster.position + CastingPosition(direction), direction, whatIsObstacle);
+        Debug.DrawRay((Vector2)caster.position + CastingPosition(direction), direction, Color.cyan);
+
+        if (hitInfo.collider.CompareTag("Player"))
+        {
+            Debug.Log("Player Found");
+        }
+        else
+        {
+            Vector3[] edgePoints = EdgePoints(hitInfo);
+            for (int i = 0; i < edgePoints.Length; i++)
+            {
+                Instantiate(edgePointPrefab, edgePoints[i], Quaternion.identity, instantiatedEdges);
+            }
+        }
+    }
+
+    private void DebugCasting(Transform target, List<GameObject> obstacles)
+    {
+        Vector3 dir = DirectionSetter(target.position, transform.position);
+        Debug.DrawRay(transform.position, dir);
+        for (int i = 0; i < obstacleTransforms.Count; i++)
+        {
+            dir = DirectionSetter(target.position, obstacleTransforms[i].transform.position);
+            Debug.DrawRay(obstacles[i].transform.position, dir, Color.red);
+        }
+    }
+
+    private Vector3 DirectionSetter(Vector3 targetPosition, Vector3 targetFrom)
+    {
+        Vector3 distance = Vector3.down;
+        if(targetPosition != null)
+        {
+            distance = targetPosition - targetFrom;
+        }
+        return distance.normalized;
+    }
+
+    private Vector2 CastingPosition(Vector2 direction)
+    {
+        float radiusX = collider2d.bounds.size.x;
+        float radiusY = collider2d.bounds.size.y;
+
+        if (direction.x > 0)
+        {
+            if (direction.y > 0)
+            {
+                return new Vector2(collider2d.bounds.extents.x + skinWidth, collider2d.bounds.extents.y + skinWidth);
+            }
+            else
+            {
+                return new Vector2(collider2d.bounds.extents.x + skinWidth, -collider2d.bounds.extents.y - skinWidth);
+            }
+        }
+        else
+        {
+            if (direction.y > 0)
+            {
+                return new Vector2(-collider2d.bounds.extents.x - skinWidth, collider2d.bounds.extents.y + skinWidth);
+            }
+            else
+            {
+                return new Vector2(-collider2d.bounds.extents.x - skinWidth, -collider2d.bounds.extents.y - skinWidth);
+            }
+        }
+    }
+
+    private Vector3[] EdgePoints(RaycastHit2D hitInfo)
+    {
+        if (hitInfo.collider.bounds.extents.x > hitInfo.collider.bounds.extents.y)
+        {
+            Vector3 edgePoint1 = new Vector2(hitInfo.collider.bounds.center.x + hitInfo.collider.bounds.extents.x + collider2d.bounds.extents.x, hitInfo.collider.bounds.center.y);
+            Vector3 edgePoint2 = new Vector2(hitInfo.collider.bounds.center.x - hitInfo.collider.bounds.extents.x - collider2d.bounds.extents.x, hitInfo.collider.bounds.center.y);
+
+            Vector3[] returnTransforms = { edgePoint1, edgePoint2 };
+            return returnTransforms;
+        }
+        else
+        {
+            Vector3 edgePoint1 = new Vector2(hitInfo.collider.bounds.center.x, hitInfo.collider.bounds.center.y + hitInfo.collider.bounds.extents.y + collider2d.bounds.extents.y);
+            Vector3 edgePoint2 = new Vector2(hitInfo.collider.bounds.center.x, hitInfo.collider.bounds.center.y - hitInfo.collider.bounds.extents.y - collider2d.bounds.extents.y);
+            Vector3[] returnTransforms = { edgePoint1, edgePoint2 };
+            return returnTransforms;
+        }
+    }
+
+}
